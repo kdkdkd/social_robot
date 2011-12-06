9be87b64f285800257492b87f5b8d6cbc80af9d7
Artem <abbbbbbbbbb@gmail.com>
2011-12-06 18:15:49 +0200

    deleted programs

--- prog/���������/����������/������� ������ � ��������� ���� ����������.rb
+++
@@ -1,9 +0,0 @@
-#Спросить о названии альбома и фотографиях
-res = ask("Название альбома" => "string", "Описание альбома" => "string", "Фотографии" => "files")
-name = res[0]
-description = res[1]
-photos = res[2]
-
-#Создать альбом и загрузить туда фото
-Album.create(name, description).upload(photos,"")
-
